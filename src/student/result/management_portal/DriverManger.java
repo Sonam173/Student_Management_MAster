590e2dba764e90a21bb667dbf0bf54f50bdcb9dd
Sonam Sahni <Sonam173>
2023-03-01 11:01:09 +0530

    first commit

---
+++ src/student/result/management_portal/DriverManger.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student.result.management_portal;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Hp
+ */
+class DriverManger {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306srm, String root, String sona123) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
